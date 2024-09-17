2b5764feb5335f6e2a3417f6b3ec6cfaeb55a9ba
Guti <gutifer666@gmail.com>
2024-09-17 17:48:19 +0200

    test: example in main

--- src/main/java/trade/javiergutierrez/dsa/DsaApplication.java
+++ src/main/java/trade/javiergutierrez/dsa/DsaApplication.java
@@ -2,12 +2,42 @@ package trade.javiergutierrez.dsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import trade.javiergutierrez.dsa._shared.GenericEntityComparator;
+import trade.javiergutierrez.dsa.dto.UserDTO;
+import trade.javiergutierrez.dsa.entity.UserEntity;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class DsaApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DsaApplication.class, args);
+        // Crear instancias de UserEntity y UserDTO
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setUsername("oldUsername");
+        userEntity.setEmail("oldEmail@example.com");
+        userEntity.setAge(30);
+        userEntity.setExtraProperty("extra");
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1L);
+        userDTO.setUsername("newUsername");
+        userDTO.setEmail("newEmail@example.com");
+        userDTO.setAge(31);
+
+        // Comparar las entidades
+        Map<String, Object[]> changes = GenericEntityComparator.compareEntities(userEntity, userDTO);
+
+        // Imprimir los cambios por consola
+        changes.forEach((fieldName, values) -> {
+            System.out.println("Campo cambiado: " + fieldName);
+            System.out.println("Valor antiguo: " + values[0]);
+            System.out.println("Valor nuevo: " + values[1]);
+            System.out.println("-------------------------");
+        });
     }
 
 }
